70a880eb6d239528452a0735547a2efc7ac9c573
Akhil Boddu <akhil1234mara@gmail.com>
2020-07-27 19:43:19 +0300

    stack array implementation complete

--- StackDemo/src/StackArray.java
+++ StackDemo/src/StackArray.java
@@ -1,5 +1,60 @@
 class StackArray {
+
+    // instance variables
+    private String[] stackArray;
+    private int top;
+
+    // constructor
+    public StackArray(int n) {
+        System.out.print("Creating StackArray\n");
+        stackArray = new String[n];
+        top = -1;
+    }
+
+    public void push(String val) {
+        top = top + 1;
+        stackArray[top] = val;
+    }
+
+    public int size() {
+        return (top + 1);
+    }
+
+    public boolean empty() {
+        return (top == -1);
+    }
+
+    public String peek() {
+        return stackArray[top];
+    }
+
+    public void print() {
+        System.out.print("Top of stack");
+        for (int i = top; i >= 0; i--) {
+            System.out.print(stackArray[i]);
+            System.out.print("\n");
+        }
+        System.out.print("End of stack");
+    }
+
     public static void main(String[] args) {
         System.out.println("Creating Stack using Array");
+
+        StackArray stack = new StackArray(10);
+        stack.push("Akhil");
+        stack.push("Boddu");
+        stack.push("Asif");
+
+        System.out.println("Peeking... " + stack.peek());
+
+        stack.push("Zolotov");
+        stack.push("Harjot");
+
+        // System.out.println(stack.empty());
+
+        stack.print();
+
+        System.out.println("Size of Array " + stack.size());
+
     }
 }
